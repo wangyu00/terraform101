800afb87df3b3fd07f6365abc640cc25abc55162
yuwang <ycwang@thoughtworks.com>
2020-07-12 16:45:55 +0800

    [wangyu] added terraform process demo.

---
+++ 01-terraform-process/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = "~> 0.12.25"
+
+  required_providers {
+    aws    = "~> 2.45"
+  }
+}
+
+provider "aws" {
+  region     = "ap-southeast-1"
+  profile    = "wangyu"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.latest_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_ami" "latest_linux" {
+  owners           = ["self", "amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["*amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
