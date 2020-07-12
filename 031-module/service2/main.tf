69058bf2dcc3dcdf19b1b4886a62725d8dc12813
yuwang <ycwang@thoughtworks.com>
2020-07-12 17:48:15 +0800

    [wangyu] added module code.

---
+++ 031-module/service2/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 0.12.25"
+
+  required_providers {
+    aws    = "~> 2.45"
+  }
+
+  backend "s3" {
+    bucket         = "xiaopo-terraform-state"
+    key            = "xiaopo_web.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "xiaopo-terraform-state-lock"
+  }
+}
+
+provider "aws" {
+  region     = "ap-southeast-1"
+  profile    = "wangyu"
+}
+
+module "ec2_for_xiaopo_service_2" {
+  source = "../module-ec2"
+
+  instance_type = "t2.small"
+}
+
